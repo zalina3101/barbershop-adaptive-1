af244b2b695e617c145913827c0d0180cf3cece7
PeterLyshak <lishak09@gmail.com>
2016-11-07 11:31:12 +0200

    Update reviews.js

--- js/reviews.js
+++ js/reviews.js
@@ -6,16 +6,18 @@ var feedbackJohn = document.querySelector('.feedback__item--john');
 var feedbackIvan = document.querySelector('.feedback__item--ivan');
 
 togglePrev.addEventListener('click', function() {
-  if (feedbackTravis.classList.contains('.feedback__item--active')) {
-    feedbackTravis.classList.remove('.feedback__item--active');
-    feedbackIvan.classList.add('.feedback__item--active');   
-      } else if (feedbackIvan.classList.contains('.feedback__item--active')) {
-        feedbackIvan.classList.remove('.feedback__item--active');
-        feedbackJohn.classList.add('.feedback__item--active'); 
-      } else if (feedbackJohn.classList.contains('.feedback__item--active')) {
-        feedbackJohn.classList.remove('.feedback__item--active');
-        feedbackTravis.classList.add('.feedback__item--active'); 
+  if (feedbackTravis.classList.contains('feedback__item--active')) {
+    feedbackTravis.classList.remove('feedback__item--active');
+    feedbackIvan.classList.add('feedback__item--active');   
+      } else { 
+      if (feedbackIvan.classList.contains('feedback__item--active')) {
+        feedbackIvan.classList.remove('feedback__item--active');
+        feedbackJohn.classList.add('feedback__item--active'); 
+      } else { if (feedbackJohn.classList.contains('feedback__item--active')) {
+        feedbackJohn.classList.remove('feedback__item--active');
+        feedbackTravis.classList.add('feedback__item--active'); 
+      }
+      }
       }
     
 });
-
